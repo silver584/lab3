cf5969c3b85a06dfeb99264dec54f09ea8809267
silver584 <2649968259@qq.com>
2025-05-07 18:05:59 +0800

    Add files via upload

--- test1/client/src/Client.java
+++ test1/client/src/Client.java
@@ -4,14 +4,17 @@ import java.util.Arrays;
 
 public class Client {
     public static void main(String[] args) {
+
+
         String host = args[0];
         int port = Integer.parseInt(args[1]);
         String filePath = args[2];
 
+        //！！！！！！！
+
         try (Socket socket = new Socket(host, port);
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-
 
             try (BufferedReader fileReader = new BufferedReader(new FileReader(filePath))) {
                 String line;
